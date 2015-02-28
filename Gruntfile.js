e968201b58df1181d65a9940adb82f3700b8f13e
Emilio Del Tessandoro <emilio.deltessa@gmail.com>
2015-02-28 15:25:48 +0100

    added grunt-sass

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+
+module.exports = function (grunt) {
+    require('load-grunt-tasks')(grunt); // npm install --save-dev load-grunt-tasks
+
+    grunt.initConfig({
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'assets/css/additions.css': 'assets/scss/additions.scss'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['sass']);
+}
